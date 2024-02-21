e5458b26d33085af05aa41ee75c7a684d5885358
aidatul rosida <rosidaaidatul@gmail.com>
2024-02-21 11:53:12 +0700

    HP

---
+++ ASD/HP.java
@@ -0,0 +1,38 @@
+public class HP {
+
+    String merk, tipe;
+    float ukuranLayar;
+    int harga;
+
+
+    public HP (String mr, String tp, float ukuran, int hrg){
+        merk =mr;
+        tipe = tp;
+        ukuranLayar =ukuran;
+        harga = hrg;
+    }
+
+    void cekKondisi(boolean c){
+        if (c == true)
+            System.out.println("HP ini second\n");
+        else
+            System.out.println("HP ini masih baru\n");
+    }
+
+    void tampilInformasi(){
+        System.out.printf("HP merk %s tipe %s dengan ukuran layar %.1f\n", merk, tipe, ukuranLayar);
+    }
+
+    public HP(){
+
+    }
+
+
+    void mengirimPesan (String pesan, String penerima, String paketData){
+        if (paketData.equalsIgnoreCase("ada")){
+            System.out.printf("Pesan %s berhasil dikirim ke %s", pesan, penerima);
+        } else{
+            System.out.printf("Pengiriman pesan ke %s gagal", penerima);
+        }
+    }
+}
